548254b190793bb0d65817bcbb261712af1026c6
aniruddha mallya <aniruddha.mallya@gmail.com>
2018-03-12 21:36:37 -0700

    Add TweetViewModel

---
+++ app/src/main/java/com/amallya/twittermvvm/data/repo/TweetListRepo.java
@@ -0,0 +1,8 @@
+package com.amallya.twittermvvm.data.repo;
+
+/**
+ * Created by anmallya on 3/12/2018.
+ */
+
+public class TweetListRepo {
+}
---
+++ app/src/main/java/com/amallya/twittermvvm/ui/tweets/TweetViewModel.java
@@ -0,0 +1,8 @@
+package com.amallya.twittermvvm.ui.tweets;
+
+/**
+ * Created by anmallya on 3/12/2018.
+ */
+
+public class TweetViewModel {
+}
